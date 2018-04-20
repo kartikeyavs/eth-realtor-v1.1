ab0515d50e614dfc6cea1ad691625e61b9f9ed43
Shiv Kartikeya <kartikeyayvs@gmail.com>
2018-04-20 05:03:45 -0700

    Update Maps.jsx

--- src/views/Maps/Maps.jsx
+++ src/views/Maps/Maps.jsx
@@ -84,7 +84,7 @@ const CustomSkinMap = withScriptjs(
 function Maps({ ...props }) {
   return (
     <CustomSkinMap
-      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAGawGx-aBfCNwfurBeOA7-2-LA1yyW4Tk"
+      googleMapURL="https://maps.googleapis.com/maps/api/js?"
       loadingElement={<div style={{ height: `100%` }} />}
       containerElement={<div style={{ height: `100vh` }} />}
       mapElement={<div style={{ height: `100%` }} />}
